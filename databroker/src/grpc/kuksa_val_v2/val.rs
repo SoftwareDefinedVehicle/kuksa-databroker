9205b2177773bb7793e406581cd47c9e90f98b27
Ruiz-Lucena Rafael (ETAS-E2E/XPC-Fe) <Rafael.Ruiz-Lucena@etas.com>
2024-07-15 19:41:57 +0200

    Initial publish value implementation

--- databroker/src/grpc/kuksa_val_v2/val.rs
+++ databroker/src/grpc/kuksa_val_v2/val.rs
@@ -11,13 +11,29 @@
 * SPDX-License-Identifier: Apache-2.0
 ********************************************************************************/
 
+use std::collections::HashMap;
 use std::pin::Pin;
+use std::sync::mpsc::Sender;
 
 use databroker_proto::kuksa::val::v2 as proto;
+use kuksa::proto::v2::open_provider_stream_request;
+use kuksa::proto::v2::open_provider_stream_response;
+use kuksa::proto::v2::OpenProviderStreamResponse;
+use kuksa::proto::v2::PublishValuesResponse;
+use tokio::{select, sync::mpsc};
+use databroker_proto::kuksa::val::v2::open_provider_stream_request::Action::PublishValuesRequest;
+use databroker_proto::kuksa::val::v2::open_provider_stream_request::Action::BatchActuateStreamResponse;
+use databroker_proto::kuksa::val::v2::open_provider_stream_request::Action::ProvidedActuation;
+use tokio_stream::wrappers::ReceiverStream;
 use tokio_stream::Stream;
 use tonic::Code;
+use tonic::Response;
+use tonic::Status;
+use tracing::debug;
 
-use crate::broker;
+use crate::broker::DataBroker;
+use crate::permissions;
+use crate::{broker::{self, AuthorizedAccess}, permissions::Permissions};
 
 #[tonic::async_trait]
 impl proto::val_server::Val for broker::DataBroker {
@@ -108,7 +124,72 @@ impl proto::val_server::Val for broker::DataBroker {
         request: tonic::Request<tonic::Streaming<proto::OpenProviderStreamRequest>>,
     ) -> Result<tonic::Response<Self::OpenProviderStreamStream>, tonic::Status>
     {
-        Err(tonic::Status::new(Code::Unimplemented, "Unimplemented"))
+        debug!(?request);
+        let permissions = match request.extensions().get::<Permissions>() {
+            Some(permissions) => {
+                debug!(?permissions);
+                permissions.clone()
+            }
+            None => return Err(tonic::Status::unauthenticated("Unauthenticated")),
+        };
+
+        let mut stream = request.into_inner();
+
+        let mut shutdown_trigger = self.get_shutdown_trigger();
+
+        // Copy (to move into task below)
+        let broker = self.clone();
+
+        // Create error stream (to be returned)
+        let (error_sender, error_receiver) = mpsc::channel(10);
+
+        // Listening on stream
+        tokio::spawn(async move {
+            let permissions = permissions;
+            loop {
+                select! {
+                    message = stream.message() => {
+                        match message {
+                            Ok(request) => {
+                                match request {
+                                    Some(req) => {
+                                        match req.action {
+                                            Some(ProvidedActuation(value)) => {
+
+                                            },
+                                            Some(PublishValuesRequest(publish_values_request)) => {
+                                                publish_values(&broker, &publish_values_request, &permissions, &error_sender);
+                                            },
+                                            Some(BatchActuateStreamResponse(value)) => {
+
+                                            },
+                                            None => {
+
+                                            },
+                                        }
+                                    },
+                                    None => {
+                                        debug!("provider: no more messages");
+                                        break;
+                                    }
+                                }
+                            },
+                            Err(err) => {
+                                debug!("provider: connection broken: {:?}", err);
+                                break;
+                            },
+                        }
+                    },
+                    _ = shutdown_trigger.recv() => {
+                        debug!("provider: shutdown received");
+                        break;
+                    }
+                }
+            }
+        });
+
+        // Return the error stream
+        Ok(Response::new(Box::pin(ReceiverStream::new(error_receiver))))
     }
 
     async fn get_server_info(
@@ -118,4 +199,52 @@ impl proto::val_server::Val for broker::DataBroker {
     {
         Err(tonic::Status::new(Code::Unimplemented, "Unimplemented"))
     }
+}
+
+async fn publish_values(
+    broker: &DataBroker,
+    request: &databroker_proto::kuksa::val::v2::PublishValuesRequest,
+    permissions: &Permissions,
+    error_sender: &Sender<Result<proto::OpenProviderStreamResponse, Status>>
+) {
+        let broker = broker.authorized_access(&permissions);
+
+        let ids: Vec<(i32, broker::EntryUpdate)> = request.datapoints
+            .iter()
+            .map(|(id, datapoint)|
+                (
+                    *id,
+                    broker::EntryUpdate {
+                        path: None,
+                        datapoint: Some(broker::Datapoint::from(datapoint)),
+                        actuator_target: None,
+                        entry_type: None,
+                        data_type: None,
+                        description: None,
+                        allowed: None,
+                        unit: None,
+                    }
+                )
+            )
+            .collect();
+
+            match broker
+                .update_entries(ids)
+                .await
+            {
+                Ok(_) => {}
+                Err(err) => {
+                    let publish_values_response = PublishValuesResponse {
+                        request_id: 123,
+                        status: HashMap::new(),
+                    };
+
+                    let action = open_provider_stream_response::Action::PublishValuesResponse(publish_values_response);
+
+                    let response = OpenProviderStreamResponse {
+                        action: Some(action),
+                    };
+                    error_sender.send(Ok(response));
+                }
+            }
 }
